247496b7084e444ffc42d1a7fba33e2f98378fe2
Andrew Zhygmanovsky <shigasumi@gmail.com>
2012-05-15 16:05:44 +0900

    DHT Chord implementation start

---
+++ src/Koneko.P2P.Chord/INodeService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace Koneko.P2P.Chord {
+	[ServiceContract]
+	public interface INodeService {
+		[OperationContract]
+		Node FindSuccessorById(long nodeId, long initialNodeId);
+		[OperationContract]
+		Node GetNodeSuccessor(
+	}
+}
--- src/Koneko.P2P.Chord/Network.cs
+++ src/Koneko.P2P.Chord/Network.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.ServiceModel;
 
 namespace Koneko.P2P.Chord {
 	public class Network {
